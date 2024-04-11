95018e4487b1cb316f29ec38b8ccb9444a6d920b
TrisGerg <gergotrischler16@gmail.com>
2024-04-11 13:40:07 +0200

    VéglegesInv/Új Mapok!
    Inventory végleges, 2db új mappal bővült.

--- Assets/Scripts/DialogueOption.cs
+++ Assets/Scripts/DialogueOption.cs
@@ -10,16 +10,53 @@ using UnityEngine.UI;
 
 public class DialogueOption : MonoBehaviour
 {
-    public GameObject uiGameObject;
-    void Start()
+    public Canvas PopUpWindow;
+    public KeyCode popupKey = KeyCode.F;
+
+    private bool canOpenPopup = false;
+
+    void OnTriggerStay2D(Collider2D other)
     {
-        uiGameObject.SetActive(false);
-    }
-    private void OnTriggerEnter(Collider player)
-    {
-        if (player.gameObject.tag == "Player")
+        if (other.tag == "Player")
         {
-            uiGameObject.SetActive(true);
+            canOpenPopup = true;
+            
+            Debug.Log("Bent vagy!");
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpenPopup = false;
+            PopUpWindow.gameObject.SetActive(false);
         }
     }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.F) && canOpenPopup)
+        {
+            PopUpWindow.gameObject.SetActive(true);
+            Debug.Log("lenyomtad!");
+        }
+        Debug.Log("Update method called");
+        if (canOpenPopup )
+        {
+            Debug.Log("Showing popup");
+            if (PopUpWindow != null)
+            {
+              
+            }
+            else
+            {
+                Debug.LogError("Popup canvas reference is null!");
+            }
+        }
+
+        
+    }
 }
+
